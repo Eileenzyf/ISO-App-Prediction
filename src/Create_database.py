75ebaad3684fa583c66b4fa531f69560607202f6
Eileenzyf <43378851+Eileenzyf@users.noreply.github.com>
2019-05-14 11:49:24 -0500

    Update Create_database.py

--- src/Create_database.py
+++ src/Create_database.py
@@ -2,6 +2,7 @@ import os
 import sys
 import logging
 import logging.config
+import pandas as pd
 
 from sqlalchemy import create_engine, Column, Integer, String, Text,Float
 from sqlalchemy.orm import sessionmaker
