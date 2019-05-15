4961fb8fac0d3e5658bf85e7591fb04fd08b1fee
Eileenzyf <43378851+Eileenzyf@users.noreply.github.com>
2019-05-14 20:55:17 -0500

    Delete upload_data.py

--- src/upload_data.py
+++
@@ -1,4 +0,0 @@
-import boto3
-s3 = boto3.client('s3')
-s3.upload_file('Appstore.csv', 'nw-eileenzhang-test', 'Appstore.csv')
-s3.upload_file('appStore_description.csv', 'nw-eileenzhang-test', 'appStore_description.csv')
