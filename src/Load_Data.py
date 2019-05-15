cbfc27c96e43bef7a47e61e2b5540447e6b37c1b
Eileenzyf <43378851+Eileenzyf@users.noreply.github.com>
2019-05-14 20:54:36 -0500

    Delete Load_Data.py

--- src/Load_Data.py
+++
@@ -1,4 +0,0 @@
-import boto3
-s3 = boto3.client('s3')
-s3.download_file('nw-eileenzhang-test', 'AppleStore.csv', 'AppStore.csv')
-s3.download_file('nw-eileenzhang-test', 'appleStore_description.csv', 'appStore_description.csv')
