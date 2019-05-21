2f4d9c60039815eeb8666dd70b5bca549faf3163
Eileenzyf <43378851+Eileenzyf@users.noreply.github.com>
2019-05-21 11:43:16 -0500

    Download data from source

---
+++ src/load_data.py
@@ -0,0 +1,29 @@
+import boto3
+import argparse
+
+s3 = boto3.client('s3')
+
+def load_data(args):
+	"""read the data from the data source
+	Args:
+		args (str): strings entered from the commend. 
+
+	Returns:
+		df (:py:class:`pandas.DataFrame`): Dowloaded csv file. 
+	"""
+	s3.download_file(args.bucket, args.filename, args.savename)
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Download data from S3")
+
+	parser.add_argument("--bucket", help="Target S3 bucket name")
+	parser.add_argument("--filename", help="Target file want to dowlaod")
+	parser.add_argument("--savename", help="Filename to be save")
+
+	args = parser.parse_args()
+	load_data(args)
+	
+
+
+
+
