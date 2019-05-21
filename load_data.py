1b76ba1279106e770ac2a4c5c4e468a2cdd2c72d
Eileenzyf <43378851+Eileenzyf@users.noreply.github.com>
2019-05-21 12:22:34 -0500

    Add comments and docstring

--- load_data.py
+++ load_data.py
@@ -4,9 +4,17 @@ import argparse
 s3 = boto3.client('s3')
 
 def load_data(args):
+	"""read the data from the data source
+	Args:
+		args (str): strings entered from the commend. 
+
+	Returns:
+		df (:py:class:`pandas.DataFrame`): Dowloaded csv file. 
+	"""
 	s3.download_file(args.bucket, args.filename, args.savename)
 
 if __name__ == "__main__":
+	#download data from the source
 	parser = argparse.ArgumentParser(description="Download data from S3")
 
 	parser.add_argument("--bucket", help="Target S3 bucket name")
