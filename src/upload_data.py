98d3fb193713e6a275fac90812bf0fd99cf18432
Eileenzyf <43378851+Eileenzyf@users.noreply.github.com>
2019-05-21 11:44:09 -0500

    Midpoint check update
    Upload File from local to S3

---
+++ src/upload_data.py
@@ -0,0 +1,25 @@
+import boto3
+import argparse
+
+s3 = boto3.client('s3')
+
+
+def upload_data(args):
+	"""upload the data to the target s3 bucket
+	Args:
+		args (str): strings entered from the commend. 
+
+	Returns:
+		None
+	"""
+	s3.upload_file(args.filename, args.bucket, args.savename)
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Upload data to S3")
+
+	parser.add_argument("--filename", help="Target file want to upload")
+	parser.add_argument("--bucket", help="Target S3 bucket name")
+	parser.add_argument("--savename", help="Filename to be save in S3")
+
+	args = parser.parse_args()
+	upload_data(args)
